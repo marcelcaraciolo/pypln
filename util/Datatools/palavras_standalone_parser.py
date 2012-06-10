0fd747d36166959dab28a3b585bcebf700e83a74
Renato Rocha Souza <rsouza.fgv@gmail.com>
2012-06-10 02:06:58 -0300

    Many bugs were fixed. Fully functional parser

--- util/Datatools/palavras_standalone_parser.py
+++ util/Datatools/palavras_standalone_parser.py
@@ -28,24 +28,46 @@ I) TIGER XML output:
 import subprocess
 import sys
 import nltk
+import os
+import glob
+import time
 from tags_dictionary import WORD_CLASSES, INF_TAGS #, SYN_TAGS, SUB_TAGS, VALENCY_TAGS
-
+ 
 PALAVRAS_ENCODING = sys.getfilesystemencoding()
 PALAVRAS_PATH = '/opt/palavras/'
+FILES_PATH = '/home/rsouza/'
 base_parser = PALAVRAS_PATH + 'por.pl'
 parser_mode = '--dep'
-malt_parser = PALAVRAS_PATH + 'bin/visldep2malt' #not used for now
-tiger_parser = PALAVRAS_PATH + 'bin/visl2tiger.pl' #not used for now
+malt_parser = PALAVRAS_PATH + 'bin/visldep2malt' #Not used for now
+tiger_parser = PALAVRAS_PATH + 'bin/visl2tiger.pl' #Not used for now
 
-def palavras_tagging(text):
+
+def files_finder(path=FILES_PATH):
+    files = []
+    print('\n__________________________________________\n')    
+    print('Preparing the set of files to process...\n')    
+    for infile in glob.glob(os.path.join(path, '*.txt') ):
+        print('Reading file:\t{0}'.format(infile))
+        files.append(infile)
+    return files
+
+
+def palavras_tagger(text):
+    t0 = time.time()
+    print('Sending to Palavras parser...\n')            
     process = subprocess.Popen([base_parser, parser_mode], 
                                stdin=subprocess.PIPE, 
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     stdout, stderr = process.communicate(text.encode(PALAVRAS_ENCODING))
+    print('Processing Palavras output...\n')            
     text_and_all_tags = []
-    text_and_pos_tags = []    
+    text_and_pos_tags = []
+    count = 0    
     for line in stdout.split('\n'):
+        count += 1
+        if count%1000 == 0:
+            print('Processing token:\t{0}'.format(count))        
         line = line.strip().decode(PALAVRAS_ENCODING)
         chunks = ''.join([chunk for chunk in line.split() if chunk.startswith('#')])
         if line.isspace() or line == '':
@@ -59,7 +81,7 @@ def palavras_tagging(text):
             else:
                 non_word_type = 'punctuation'
             text_and_all_tags.append(['non word', non_word, non_word_type, '', '', '', chunks])
-        elif len(line.split('\t')) < 2:  #discard malformed lines
+        elif len(line.split('\t')) < 2:  #Discard malformed lines
             continue
         else:
             word = line.split('\t')[0].strip()    
@@ -72,10 +94,14 @@ def palavras_tagging(text):
             text_and_all_tags.append([word, lemma, pos_tag, secondary_tag, inflexion_tag, syntactic_tag, chunks])
     num_tokens = len(text_and_all_tags)
     for position in range(num_tokens):
-        text_and_pos_tags.append((text_and_all_tags[position][0], text_and_all_tags[position][2]))    
+        text_and_pos_tags.append((text_and_all_tags[position][0], text_and_all_tags[position][2]))
+    t1 = time.time() - t0
+    print('\n{0} tokens processed in {1:.2f} seconds ({2:.1f} tokens/second)\n'.format(count,t1,(count//t1)))
     return text_and_all_tags, text_and_pos_tags #palavras style, nltk style
 
-def np_extraction(text_and_pos_tags):
+
+def np_extractor(text_and_pos_tags):
+    # These rules may be fine tuned to catch smallers or bigger NPs    
     nprules = r'''
         NP: {(<DET>+<ADJ>+<KC>+)*<ADJ|ADV|DET|NUM>*<N>+(<ADJ|ADV|DET|NUM|PRP|SPEC>*<N>+)*(<KC>+<ADJ|ADV|NUM>+)*}
             {(<DET>+<ADJ>+<KC>+)*<ADJ|ADV|DET|NUM>*<N>+<ADJ|ADV|NUM>*(<KC>+<ADJ|ADV|NUM>+)*}
@@ -89,7 +115,6 @@ def np_extraction(text_and_pos_tags):
     for np in np_trees:
         l = [word for word, tag in np]
         np_list.append(l)
-    #word, tag = zip(*noun_phrases[795])
     return np_list    
 
 
@@ -101,12 +126,27 @@ def chunks2strings(chunks):
 
 
 if __name__ == '__main__':
-    txtfile = '/home/rsouza/3110202.txt'#file.txt'    
-    document_text = open(txtfile,'r').read().decode(PALAVRAS_ENCODING)
-    parsed_text = palavras_tagging(document_text)
-    noun_phrases = np_extraction(parsed_text[1])
-    noun_phrases = chunks2strings(noun_phrases)
-    for np in noun_phrases: 
-        print np
-    fd = nltk.FreqDist(word.lower() for word in noun_phrases)
-    fd.plot(30)
+    txt_files_in_dir = files_finder()
+    for txt_file in txt_files_in_dir:
+        print('____________________________________________\n')
+        print('Processing file:\t{0}\n'.format(txt_file))
+        document_text = open(txt_file,'r').read().decode(PALAVRAS_ENCODING)
+        parsed_text = palavras_tagger(document_text)
+        noun_phrases = np_extractor(parsed_text[1])
+        noun_phrases = chunks2strings(noun_phrases)
+        noun_phrases = [np for np in noun_phrases if len(np)>1] #Filtering single letters
+        print('Saving NPs to file:\t{0}_np.out'.format(txt_file))        
+        f = open('{0}_np.out'.format(txt_file), 'w')
+        f.write('\n *** Noun Phrases on file:\t{0} *** \n\n'.format(txt_file))
+        for np in noun_phrases: 
+            #print(np)
+            f.write('\n%s' % np)
+        f.write('\n\n *** NP Frequencies on file:\t{0} ***\n\n'.format(txt_file))
+        fd = nltk.FreqDist(word.lower() for word in noun_phrases)
+        for np, freq in fd.items():
+            f.write('\nNP:\t{0}\t{1}'.format(np,freq))
+        #fd.plot(30)
+        f.close()
+        print('\nClosing file:\t{0}_np.out\n'.format(txt_file))     
+        
+        
